b638bf1d62a23615d6110e9f6ad8513d1a890097
Pindi Albert <pindi.albert@gmail.com>
2011-06-16 14:57:43 -0400

    Write command line interface

--- genetrack/genetrack.py
+++ genetrack/genetrack.py
@@ -1,4 +1,5 @@
-import csv, logging, numpy, math, bisect
+from optparse import OptionParser, IndentedHelpFormatter
+import csv, logging, numpy, math, bisect, sys, os
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
@@ -65,7 +66,7 @@ def normal_array(width, sigma, normalize=True):
 
     return values
 
-def call_peaks(array, keys, direction, width=WIDTH, exclusion=0):
+def call_peaks(array, data, keys, direction, width=WIDTH, exclusion=0):
     # Go through the array and call each peak
     peaks = []
     history = [array[0], array[1]] # Last 2 values
@@ -81,6 +82,8 @@ def call_peaks(array, keys, direction, width=WIDTH, exclusion=0):
         reads = get_window(data, peak.start, peak.end, keys)
         peak.value = sum([read[direction] for read in reads])
         
+    before = len(peaks)
+        
     # Process the exclusion zone
     peak_keys = make_peak_keys(peaks)
     peaks_by_value = peaks[:]
@@ -94,37 +97,109 @@ def call_peaks(array, keys, direction, width=WIDTH, exclusion=0):
             i = get_index(excluded.index, peak_keys)
             del peak_keys[i]
             del peaks[i]
+            
+    after = len(peaks)
+    logging.debug('%d of %d peaks (%d%%) survived exclusion' % (after, before, after*100/before))
+            
     return peaks
     
-        
-chromosomes = parse_reads(csv.reader(open('data/INPUT_genetrack_Reb1_rep2.idx','rU'), delimiter='\t'))
-writer = csv.writer(open('data/OUTPUT.txt', 'wt'), delimiter='\t')
-writer.writerow(('chrom', 'strand', 'start', 'end', 'value'))
-
-for cname, data in chromosomes.items():
-    cname = 'chr01'
-    data = chromosomes[cname]
-    keys = make_keys(data)
-    # Create the arrays that hold the sum of the normals
-    forward_array = allocate_array(data, WIDTH)
-    reverse_array = allocate_array(data, WIDTH)
-    normal = normal_array(WIDTH, 10)
+def process_file(path, sigma, exclusion):
     
-    # Add each read's normal to the array
-    for read in data:
-        index, forward, reverse = read
-        # Add the normals to the appropriate regions
-        forward_array[index:index+WIDTH*2] += normal * forward
-        reverse_array[index:index+WIDTH*2] += normal * reverse
-        
-    forward_peaks = call_peaks(forward_array, keys, 1)
-    reverse_peaks = call_peaks(reverse_array, keys, 2)
-
+    logging.info('Processing file "%s"' % path)
     
-    for peak in forward_peaks:
-        writer.writerow((cname, '+', peak.start, peak.end, peak.value))
-    for peak in reverse_peaks:
-        writer.writerow((cname, '-', peak.start, peak.end, peak.value))   
+    directory, fname = os.path.split(path)
+    
+    if fname.startswith('INPUT'):
+        fname = fname[5:].strip('_') # Strip "INPUT_" from the file if present
+    fname = ''.join(fname.split('.')[:-1]) # Strip extension (will be re-added as appropriate)
 
-    print forward_peaks[:100]
-    break
+    output_dir = os.path.join(directory, 'genetrack_s%de%d' % (sigma, exclusion))
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    output_path = os.path.join(output_dir, fname+'O_%s.txt' % fname)
+    
+    chromosomes = parse_reads(csv.reader(open(path,'rU'), delimiter='\t'))
+    writer = csv.writer(open(output_path, 'wt'), delimiter='\t')
+    writer.writerow(('chrom', 'strand', 'start', 'end', 'value'))
+    
+    for cname, data in chromosomes.items():
+        logging.debug('Processing chromosome %s' % cname)
+        data = chromosomes[cname]
+        keys = make_keys(data)
+        # Create the arrays that hold the sum of the normals
+        forward_array = allocate_array(data, WIDTH)
+        reverse_array = allocate_array(data, WIDTH)
+        normal = normal_array(WIDTH, sigma)
+        
+        # Add each read's normal to the array
+        for read in data:
+            index, forward, reverse = read
+            # Add the normals to the appropriate regions
+            forward_array[index:index+WIDTH*2] += normal * forward
+            reverse_array[index:index+WIDTH*2] += normal * reverse
+            
+        logging.debug('Calling forward strand')
+        forward_peaks = call_peaks(forward_array, data, keys, 1, exclusion=exclusion)
+        logging.debug('Calling reverse strand')
+        reverse_peaks = call_peaks(reverse_array, data, keys, 2, exclusion=exclusion)
+    
+        
+        for peak in forward_peaks:
+            writer.writerow((cname, '+', peak.start, peak.end, peak.value))
+        for peak in reverse_peaks:
+            writer.writerow((cname, '-', peak.start, peak.end, peak.value))   
+    
+
+usage = '''
+input_paths may be:
+- a file or list of files to run on
+- a directory or list of directories to run on all files in them
+- "." to run in the current directory
+
+example usages:
+python genetrack.py -s 10 /path/to/a/file.txt path/to/another/file.txt
+python genetrack.py -s 5 -e 50 /path/to/a/data/directory/
+python genetrack.py .
+'''.lstrip()
+
+
+ 
+# We must override the help formatter to force it to obey our newlines in our custom description
+class CustomHelpFormatter(IndentedHelpFormatter):
+    def format_description(self, description):
+        return description
+
+
+def run():   
+    parser = OptionParser(usage='%prog [options] input_paths', description=usage, formatter=CustomHelpFormatter())
+    parser.add_option('-s', action='store', type='int', dest='sigma', default=5,
+                      help='Sigma to use when smoothing reads to call peaks. Default 5.')
+    parser.add_option('-e', action='store', type='int', dest='exclusion', default=20,
+                      help='Exclusion zone around each peak that prevents others from being called. Default 20.')
+    parser.add_option('-v', action='store_true', dest='verbose', help='Verbose mode: displays debug messages')
+    parser.add_option('-q', action='store_true', dest='quiet', help='Quiet mode: suppresses all non-error messages')
+    (options, args) = parser.parse_args()
+    
+    if options.verbose:
+        logging.getLogger().setLevel(logging.DEBUG) # Show all info/debug messages
+    if options.quiet:
+        logging.getLogger().setLevel(logging.ERROR) # Silence all non-error messages
+        
+    if not args:
+        parser.print_help()
+        sys.exit(1)
+        
+    for path in args:
+        if not os.path.exists(path):
+            parser.error('Path %s does not exist.' % path)
+        if os.path.isdir(path):
+            for fname in os.listdir(path):
+                fpath = os.path.join(path, fname)
+                if os.path.isfile(fpath) and not fname.startswith('.'): 
+                    process_file(fpath, options.sigma, options.exclusion)
+        else:
+            process_file(path, options.sigma, options.exclusion)
+            
+if __name__ == '__main__':
+    run()
+            
