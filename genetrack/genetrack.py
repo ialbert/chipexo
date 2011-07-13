d39c23e40f06d897de4c423adf38048d161cbb43
Pindi Albert <pindi.albert@gmail.com>
2011-07-13 16:42:59 -0400

    Begin functions to slice data into chunks

--- genetrack/genetrack.py
+++ genetrack/genetrack.py
@@ -114,11 +114,35 @@ def get_index(value, keys):
     ''' Returns the index of the value in the keys using bisect '''
     return bisect.bisect_left(keys, value)
 
-def allocate_array(data, width=0):
-    ''' Allocated a new array with the dimensions required to fit all reads in the
-    argument. The new array is totally empty.'''
+def get_range(data):
+    lo = min([item[0] for item in data])
     hi = max([item[0] for item in data])
-    return numpy.zeros(hi+width*2, numpy.float)
+    return lo, hi
+
+def get_chunks(lo, hi, size, overlap=100):
+    ''' Divides a range into chunks of maximum size size. Returns a list of 2-tuples
+    (slice_range, process_range), each a 2-tuple (start, end). process_range has zero overlap
+    and should be given to process_chromosome as-is, and slice_range is overlapped and should be used to
+    slice the data (using get_window) to be given to process_chromosome. '''
+    chunks = []
+    for start_index in range(lo, hi, size):
+        process_start = start_index
+        process_end = min(start_index + size, hi) # Don't go over upper bound
+        slice_start = max(process_start - overlap, lo) # Don't go under lower bound
+        slice_end = min(process_end + overlap, hi) # Don't go over upper bound
+        chunks.append(((slice_start, slice_end), (process_start, process_end)))
+    return chunks
+    
+    
+
+def allocate_array(data, width):
+    ''' Allocates a new array with the dimensions required to fit all reads in the
+    argument. The new array is totally empty. Returns the array and the shift (number to add to
+    a read index to get the position in the array it should be at).'''
+    lo, hi = get_range(data)
+    rng = hi - lo
+    shift = width - lo
+    return numpy.zeros(rng+width*2, numpy.float), width - lo
     
 def normal_array(width, sigma, normalize=True):
     ''' Returns an array of the normal distribution of the specified width '''
@@ -138,7 +162,7 @@ def normal_array(width, sigma, normalize=True):
 
     return values
 
-def call_peaks(array, data, keys, direction, options):
+def call_peaks(array, shift, data, keys, direction, options):
     peaks = []
     def find_peaks():
         # Go through the array and call each peak
@@ -149,13 +173,13 @@ def call_peaks(array, data, keys, direction, options):
             neg = options.up_width or options.exclusion // 2
             if direction == 2: # Reverse strand
                 pos, neg = neg, pos # Swap positive and negative widths
-            peaks.append(Peak(int(index)-WIDTH, pos, neg))
+            peaks.append(Peak(int(index)-shift, pos, neg))
     find_peaks()
         
     def calculate_reads():
         # Calculate the number of reads in each peak
         for peak in peaks:
-            reads = get_window(data, peak.start, peak.end, keys)
+            reads = get_window(data, peak.start+shift, peak.end+shift, keys)
             peak.value = sum([read[direction] for read in reads])
     calculate_reads()
         
@@ -182,13 +206,15 @@ def call_peaks(array, data, keys, direction, options):
             
     return peaks
     
-def process_chromosome(cname, data, writer, options):
+def process_chromosome(cname, data, writer, process_bounds, options):
+    ''' Process a chromosome. Takes the chromosome name, list of reads, a CSV writer
+    to write processes results to, the bounds (2-tuple) to write result in, and options. '''
     
     logging.debug('Processing chromosome %s' % cname)
     keys = make_keys(data)
     # Create the arrays that hold the sum of the normals
-    forward_array = allocate_array(data, WIDTH)
-    reverse_array = allocate_array(data, WIDTH)
+    forward_array, forward_shift = allocate_array(data, WIDTH)
+    reverse_array, reverse_shift = allocate_array(data, WIDTH)
     normal = normal_array(WIDTH, options.sigma)
     
     
@@ -198,15 +224,15 @@ def process_chromosome(cname, data, writer, options):
             index, forward, reverse = read
             # Add the normals to the appropriate regions
             if forward:
-                forward_array[index:index+WIDTH*2] += normal * forward
+                forward_array[index+forward_shift-WIDTH:index+forward_shift+WIDTH] += normal * forward
             if reverse:
-                reverse_array[index:index+WIDTH*2] += normal * reverse
+                reverse_array[index+reverse_shift-WIDTH:index+reverse_shift+WIDTH] += normal * reverse
     populate_array()
         
     logging.debug('Calling forward strand')
-    forward_peaks = call_peaks(forward_array, data, keys, 1, options)
+    forward_peaks = call_peaks(forward_array, forward_shift, data, keys, 1, options)
     logging.debug('Calling reverse strand')
-    reverse_peaks = call_peaks(reverse_array, data, keys, 2, options)
+    reverse_peaks = call_peaks(reverse_array, reverse_shift, data, keys, 2, options)
 
     # Convert chromosome name in preparation for writing our
     cname = convert_data(cname, 'zeropad', 'numeric')
@@ -250,7 +276,11 @@ def process_file(path, options):
     
     for cname, data in chromosome_iterator(reader):
         if not options.chromosome or options.chromosome == cname:
-            process_chromosome(cname, list(data), writer, options)
+            print cname
+            data = list(data)
+            lo, hi = get_range(data)
+            print get_chunks(lo, hi, size=100000, overlap=WIDTH)
+            #process_chromosome(cname, list(data), writer, options)
         else:
             list(data) # Consume iterator even if not used
     
